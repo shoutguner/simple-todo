344f1a61840bcb207b6d74d0ca478640ce3974b2
Maxim Malinovsky <malinovsky.dev@gmail.com>
2017-04-12 16:58:18 +0300

    Update editTaskText_spec.rb

--- spec/features/tasks/editTaskText_spec.rb
+++ spec/features/tasks/editTaskText_spec.rb
@@ -24,6 +24,7 @@ feature 'Edit task text', js: true do
     page.find('#task_id_0_edit').click
 
     expect(page).to have_content('Another text')
+    screenshot_and_save_page
     expect(page).to have_content('Fake expectation')
   end
 
