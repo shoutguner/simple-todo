d6ffa37a4242fc2f331a940efd9df6c975b30d71
Maxim Malinovsky <malinovsky.dev@gmail.com>
2017-06-08 11:59:12 +0300

    Delete download_route_spec.rb

--- spec/routing/download_route_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe 'routes to the downloads controller', :type => :routing do
-  it 'should route to downloads#download' do
-    expect(get: '/tmp/uploads/comment/attachments/1/file.mp3')
-        .to route_to(controller: 'downloads', action: 'download', comment_id: '1', file_name: 'file', format: 'mp3')
-  end
-end
