4f46f7a3be9658fe4fa9e7c454bb724bcc045db5
Maxim Malinovsky <malinovsky.dev@gmail.com>
2017-06-08 11:59:06 +0300

    Delete root_route_spec.rb

--- spec/routing/root_route_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe 'routes to the root path', :type => :routing do
-  it 'should route to projects#root' do
-    expect(get: root_path).to route_to(controller: 'projects', action: 'root')
-  end
-end
